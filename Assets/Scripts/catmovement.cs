e97114a79add9c2fcb1b3c7306b6e8d8c203851c
tobybartclev <56485540+tobybartclev@users.noreply.github.com>
2020-02-29 15:21:08 -0500

    Added cat targeting mechanic to catmovement script

--- Assets/Scripts/catmovement.cs
+++ Assets/Scripts/catmovement.cs
@@ -11,6 +11,12 @@ public class catmovement : MonoBehaviour
     //Turn interval value; how many seconds to move during movement phase before deciding on a turn movement
     public float turninterval = 2f;
 
+    //How many times to walk around before trying to acquire a target
+    public int targetinterval;
+
+    //Timer variable to count down until cat targets something
+    public int targettimer = 0;
+
     //Targetting boolean to check if cat is going after a breakable
     public bool targetting = false;
 
@@ -19,6 +25,9 @@ public class catmovement : MonoBehaviour
 
     //"Heading"; a rotation value in degrees from 0 to 360 (rotating around y-axis)
     public float heading = 0f;
+
+    //Game object that will be target of cat
+    GameObject currenttarget;
 
     //Declare rigidbody
     Rigidbody rigidbody;
@@ -33,6 +42,9 @@ public class catmovement : MonoBehaviour
         //heading = Random.Range(0, 360);
         heading = 0;
 
+        //Set a random targetting interval, so cats don't always acquire targets at the same time
+        targetinterval = 2;
+
         //Rotate cat to face movement direction
         transform.Rotate(0, heading, 0);
 
@@ -43,6 +55,9 @@ public class catmovement : MonoBehaviour
         StartCoroutine(TurnTimer());
     }
 
+    //Timer for how long to walk before stopping to turn
+    //Also iterates the timer for the targeting function
+    //Calls Target() when timer hits target, otherwise calls Turn()
     IEnumerator TurnTimer()
     {
         //Wait a few seconds before starting the next turn
@@ -50,11 +65,26 @@ public class catmovement : MonoBehaviour
 
         //Set walking to false to stop cat for turn
         walking = false;
+        
+        //Increase target timer
+        targettimer += 1;
 
-        //Make the cat turn by starting void Turn
-        StartCoroutine(Turn());
+        //Check targeting timer
+        if (targettimer == targetinterval)
+        {
+            //Initiate targeting systems
+            Target();
+        }
+
+        else
+        {
+            //Make the cat turn by starting void Turn
+            StartCoroutine(Turn());
+        }
+
     }
 
+    //Turns the cat and restarts the turn timer
     IEnumerator Turn()
     {
         //Make the cat stop for a second when turning
@@ -71,6 +101,44 @@ public class catmovement : MonoBehaviour
 
         //Restart the timer to keep the wandering going
         StartCoroutine(TurnTimer());
+    }
+    
+    //Chooses a target for the cat
+    //Currently the closest target
+    void Target()
+    {
+        //Array for targeting
+        GameObject[] targets;
+        
+        //Populates array with all breakables in scene
+        targets = GameObject.FindGameObjectsWithTag("Breakable");
+
+        //Variables for upcoming for loop
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+
+        //Goes through each breakable in scene
+        //Keeps closest object so far, until all objects are iterated
+        foreach (GameObject target in targets)
+        {
+            Vector3 diff = target.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = target;
+                distance = curDistance;
+            }
+        }
+
+        //Assigns target as "closest" from above
+        currenttarget = closest;
+
+        //Make cat face the target
+        transform.LookAt(currenttarget.transform);
+
+        //Sets the cat to walking
+        walking = true;
     }
 
     // Update is called once per frame
