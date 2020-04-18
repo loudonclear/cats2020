41b44cfcad1c04fe63d3f82624a548e88207562d
tobybartclev <56485540+tobybartclev@users.noreply.github.com>
2020-04-18 16:08:21 -0400

    Added scripts for updating TV screen with new values

---
+++ Assets/Scripts/Breakage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakage : MonoBehaviour
+{
+
+    public int itemvalue;
+
+    GameObject tv;
+
+    GameObject tvscreen;
+
+    TVmessage TVmessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tv = GameObject.Find("tv");
+        tvscreen = GameObject.Find("Description");
+        TVmessage = tvscreen.GetComponent<TVScreen>();
+        itemvalue = 500;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy()
+    {
+        TVmessage.value -= itemvalue;
+    }
+}
---
+++ Assets/Scripts/TVScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TVScreen : MonoBehaviour
+{
+    public int value;
+
+    private Text message;
+
+    private string tempmessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        value = 5000;
+        message = GetComponent<Text>();
+        tempmessage = "Room value: $" + value.ToString();
+        message.text = tempmessage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempmessage = "Room value: $" + value.ToString();
+        message.text = tempmessage;
+    }
+}
