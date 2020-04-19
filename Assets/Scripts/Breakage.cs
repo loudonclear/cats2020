ed61765b85bf54acfad73a73160705be8c225305
tobybartclev <56485540+tobybartclev@users.noreply.github.com>
2020-04-18 20:42:08 -0400

    Added comments to the scripts for breakage and the tv screen display

--- Assets/Scripts/Breakage.cs
+++ Assets/Scripts/Breakage.cs
@@ -5,20 +5,26 @@ using UnityEngine.UI;
 
 public class Breakage : MonoBehaviour
 {
-
+    //The item's specific value in dollars
     public int itemvalue;
 
+    //The tv object from the scene
     GameObject tv;
 
+    //The text object from the tv from the scene
     GameObject tvscreen;
-
-    //TVmessage TVmessage;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Get the tv object
         tv = GameObject.Find("tv");
+
+        //Get the tv text object
         tvscreen = GameObject.Find("Description");
+
+        //Set the item's value (might want to delete and modify in the editor once
+        //objects with different values are added)
         itemvalue = 500;
     }
 
@@ -28,8 +34,12 @@ public class Breakage : MonoBehaviour
         
     }
 
+    // When the object is destroyed by the cats (may replace trigger depending on
+    // breakage mechanics later)
     void OnDestroy()
     {
+        //Subtract the item's dollar value from the value on the tv text object
+        //This updates the tv with a new, lower room value
         tvscreen.GetComponent<TVScreen>().value -= itemvalue;
     }
 }
--- Assets/Scripts/TVScreen.cs
+++ Assets/Scripts/TVScreen.cs
@@ -5,17 +5,25 @@ using UnityEngine.UI;
 
 public class TVScreen : MonoBehaviour
 {
+    //The dollar value of the room
     public int value;
 
+    //The text element of the tv
     private Text message;
 
+    //The message to be put up on the tv
     private string tempmessage;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Set the starting value for the room
         value = 5000;
+
+        //Get the text component
         message = GetComponent<Text>();
+
+        //Generate and post the starting message (can be changed later if necessary)
         tempmessage = "Room value: $" + value.ToString();
         message.text = tempmessage;
     }
@@ -23,6 +31,7 @@ public class TVScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Update the tempmessage and post it to the tv text component
         tempmessage = "Room value: $" + value.ToString();
         message.text = tempmessage;
     }
