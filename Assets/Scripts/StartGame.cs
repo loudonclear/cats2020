acdeedd4803137ca384b1f39fcc0f35b89e96669
loudonclear <loudon_cohen@brown.edu>
2020-05-02 19:10:18 -0400

    Fixed textured objects, added menu

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class StartGame : MonoBehaviour
+{
+    public GameObject[] menu;
+
+    private 
+    void Start()
+    {
+        GetComponent<VRTK_InteractableObject>().InteractableObjectTouched += new InteractableObjectEventHandler(ObjectGrabbed);
+        //GetComponent<VRTK_InteractableObject>().InteractableObjectGrabbed += new InteractableObjectEventHandler(ObjectGrabbed);
+    }
+
+    private void ObjectGrabbed(object sender, InteractableObjectEventArgs e)
+    {
+        foreach (GameObject obj in menu)
+        {
+            obj.SetActive(false);
+        }
+    }
+}
